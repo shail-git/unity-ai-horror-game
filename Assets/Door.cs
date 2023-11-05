5bc35f19b02f068a8da379c15378f5e86558a9a1
shail-git <shail.shah@sakec.ac.in>
2023-11-05 03:31:57 -0500

    project files

---
+++ Assets/Door.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject door_closed, door_opened;
+    public AudioSource open, close;
+    public bool opened;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("MainCamera")){
+            if (opened == false)
+            {
+                // intText.SetActive(true);
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    door_closed.SetActive(false);
+                    door_opened.SetActive(true);
+                    // intText.SetActive(false);
+                    //open.Play()
+                    StartCoroutine(repeat());
+                    opened = true;
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("MainCamera"))
+        {
+            // intText.SetActive(false);
+        }
+    }
+
+    IEnumerator repeat()
+    {
+        yield return new WaitForSeconds(4.0f);
+        opened = false;
+        door_closed.SetActive(true);
+        door_opened.SetActive(false);
+        //close.Play()
+    }
+}
